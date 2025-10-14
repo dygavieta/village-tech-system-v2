da684e6a51205245401d2126fa820773d41e2746
Don Michael Gavieta <don.gavieta@98labs.com>
2025-10-14 11:27:08 +0800

    build setup fixes for residence app

--- apps/residence/lib/core/theme/app_theme.dart
+++ apps/residence/lib/core/theme/app_theme.dart
@@ -22,10 +22,10 @@ class AppTheme {
       centerTitle: true,
       elevation: 0,
     ),
-    cardTheme: CardTheme(
+    cardTheme: const CardThemeData(
       elevation: 2,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
     ),
     inputDecorationTheme: InputDecorationTheme(
@@ -55,10 +55,10 @@ class AppTheme {
       centerTitle: true,
       elevation: 0,
     ),
-    cardTheme: CardTheme(
+    cardTheme: const CardThemeData(
       elevation: 2,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
     ),
     inputDecorationTheme: InputDecorationTheme(
